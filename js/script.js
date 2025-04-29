f292ec98e8d1c7107ca35a6be11acf60f8bbacf5
Lynn Lukose <lynn.lukose@stu.ocsb.ca>
2025-04-29 16:43:40 +0000

    added js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lynn Lukose
+// Created on: April 2025
 // This file contains the JS functions for index.html
+const randomNumber = Math.floor(Math.random() * 6) + 1
+/**
+ * This function generates random positive or negative number
+ */
+
+function generateNumber() {
+  // input
+  const optionPositive = document.getElementById("option-positive").checked
+
+  // generate a new random number every time
+  const randomNumber = Math.floor(Math.random() * 6) + 1
+
+  // output
+  if (optionPositive === true) {
+    document.getElementById("answer").innerHTML =
+      "Your random number is: " + randomNumber
+  } else {
+    const randomNegativeNumber = randomNumber * -1
+    document.getElementById("answer").innerHTML =
+      "Your random number is: " + randomNegativeNumber
+  }
+}
